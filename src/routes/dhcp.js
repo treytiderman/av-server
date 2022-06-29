61b5f0fc7ae046d9bdc280468161163731c52206
TreyTiderman <tiderman.trey@gmail.com>
2022-06-28 20:18:39 -0500

    dhcp server routes

---
+++ src/routes/dhcp.js
@@ -0,0 +1,42 @@
+// Create Express router
+const express = require('express');
+const router = express.Router();
+
+// Controls
+const dhcp = require("../modules/dhcp");
+
+// Routes
+router.get('/clients', (req, res) => {
+  res.json(dhcp.clients);
+});
+router.get('/serverRunning', (req, res) => {
+  res.json(dhcp.serverRunning);
+});
+router.get('/serverOptions', (req, res) => {
+  res.json(dhcp.serverOptions);
+});
+router.get('/start', (req, res) => {
+  const output = dhcp.start(dhcp.serverOptions)
+  res.status(200).json(output);
+});
+router.get('/stop', (req, res) => {
+  const output = dhcp.stop()
+  res.status(200).json(output);
+});
+router.post('/serverOptions', (req, res) => {
+  const options = JSON.parse(req.body);
+  const output = dhcp.setServerOptions(
+    options.ip,
+    options.rangeStart,
+    options.rangeStop,
+    options.mask,
+    options.gateway,
+    options.dns1,
+    options.dns2,
+    options.leaseTime
+  )
+  res.status(200).json(output);
+});
+
+// Export
+exports.router = router;
