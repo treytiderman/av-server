961fea6cbcec16dbb2e1690980391b009be60072
Trey Tiderman <tiderman.trey@gmail.com>
2023-12-30 16:13:45 -0600

    add todo

--- server/TODO.md
+++ server/TODO.md
@@ -1,6 +1,7 @@
 # TODO
 
 - [ ] program-v1.js parsing a env file crashes with blank lines
+- [ ] program-v1.js available programs in nested folders
 
 
 
--- server/server.js
+++ server/server.js
@@ -7,11 +7,11 @@ import { create as createWsServer } from './core/websocket-server.js'
 import { createServer as createHttpServer } from 'http'
 const server = createWsServer(createHttpServer(expressServer))
 
-// Run Tests
-import "./test.js"
-
 // API
 import "./routes.js"
+
+// Run Tests
+import "./test.js"
 
 // Start Server
 const port = process.env.PORT || 4620
